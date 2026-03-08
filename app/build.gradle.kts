3e3ca3007796cb24a691e56050dae8b1171f8457
Votre Nom <votre.email@exemple.com>
2026-03-08 08:47:44 +0100

    Update version to 0.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 10
-        versionName = "1.0.9"
+        versionName = "0.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
